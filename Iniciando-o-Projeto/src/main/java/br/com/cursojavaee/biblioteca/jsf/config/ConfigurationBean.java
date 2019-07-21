44c4ee56f6c7a3f56499f897a860081b7511412e
Odenilton Lima <odeniltonlj@gmail.com>
2019-07-21 12:31:13 -0300

    Initial Commit

---
+++ Iniciando-o-Projeto/src/main/java/br/com/cursojavaee/biblioteca/jsf/config/ConfigurationBean.java
@@ -0,0 +1,15 @@
+package br.com.cursojavaee.biblioteca.jsf.config;
+
+/**
+ * Created by xinyuan.zhang on 9/4/17.
+ */
+import static javax.faces.annotation.FacesConfig.Version.JSF_2_3;
+
+import javax.faces.annotation.FacesConfig;
+
+@FacesConfig(
+		// Activates CDI build-in beans
+		version = JSF_2_3)
+public class ConfigurationBean {
+
+}
